f00004fbbeec249166d5d29a175ca887d26bc3af
Adel Naim <>
2024-08-23 06:33:55 +0300

    V 1.0.0

---
+++ omni_A93.mk
@@ -0,0 +1,34 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Otherwise, If you have 32-bit device, add the below line instead of above line
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Replace $$DEVICE$$ with your Device Name's Value.
+# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
+PRODUCT_COPY_FILES += device/$$BRAND$$/$$DEVICE$$/prebuilt/zImage:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
+PRODUCT_BRAND := OPPO
+PRODUCT_DEVICE := CPH2121
+PRODUCT_MANUFACTURER := OPPO
+PRODUCT_NAME := omni_A93
+PRODUCT_MODEL := OPPO A93
+
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    # These lines are from my device. You MUST Replace yours.
+    BUILD_FINGERPRINT="OPPO/A93/A93:10.0/QP1A.190711.020/0c06c9d8c13af774:user/release-keys"
+    PRIVATE_BUILD_DESC="full_oppo6779-user 10 QP1A.190711.020 0c06c9d8c13af774 release-keys"
